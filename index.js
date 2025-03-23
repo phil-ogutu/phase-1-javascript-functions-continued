d004a5e0655b5f7b6eec73106fc162b6f264a24d
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-23 16:41:28 +0300

    Functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+// console.log(saturdayFun())
+// console.log(saturdayFun("Play"))
+
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+// console.log(mondayWork())
+// console.log(mondayWork("play"))
+
+function wrapAdjective(param = "*"){
+    return function(arg = "special"){
+        return `You are ${param}${arg}${param}!`
+    } 
+}
+
+// console.log(wrapAdjective("!!!")("a dedicated programmer"))
